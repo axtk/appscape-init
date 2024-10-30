91fff15fa5a15f0562ad27565521b1fd6c5ea343
axtk <axtkx@inbox.ru>
2024-10-30 14:17:50 +0300

    update demo render markup

--- demo/src/entries/intro/render.ts
+++ demo/src/entries/intro/render.ts
@@ -1,17 +1,18 @@
 import type {Controller} from 'appscape';
 
 export const render: Controller = () => {
-    return (_req, res) => {
+    return (req, res) => {
+        let nonce = req.ctx?.nonce;
+
         res.send(
             '<!DOCTYPE html>' +
             '<html><head><meta charset="utf-8"/>' +
             '<meta name="viewport" content="width=device-width"/>' +
             '<link type="image/x-icon" rel="icon" href="/favicon.svg"/>' +
-            '<title>Well met!</title></head>' +
-            '<body><main style="text-align: center;">' +
-            '<h1>Well met!</h1><hr/>' +
-            '<p><em>appscape</em></p>' +
-            '</main></body></html>\n',
+            '<title>Well met!</title>' +
+            `<style${nonce ? ` nonce="${nonce}"` : ''}>body{text-align:center;}</style>` +
+            '</head><body><h1>Well met!</h1><hr/>' +
+            '<p><em>appscape</em></p></body></html>\n',
         );
     };
 };
