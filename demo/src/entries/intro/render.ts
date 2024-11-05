4da251d236e6bd968228b9817210b8a3ab5722d6
axtk <axtkx@inbox.ru>
2024-11-05 15:38:32 +0300

    update demo content

--- demo/src/entries/intro/render.ts
+++ demo/src/entries/intro/render.ts
@@ -9,9 +9,9 @@ export const render: Controller = () => {
             '<html><head><meta charset="utf-8"/>' +
             '<meta name="viewport" content="width=device-width"/>' +
             '<link type="image/x-icon" rel="icon" href="/favicon.svg"/>' +
-            '<title>Well met!</title>' +
+            '<title>✔️ Well met!</title>' +
             `<style${nonce ? ` nonce="${nonce}"` : ''}>body{text-align:center;}</style>` +
-            '</head><body><h1>Well met!</h1><hr/>' +
+            '</head><body><h1>✔️ Well met!</h1><hr/>' +
             '<p><em>appscape</em></p></body></html>\n',
         );
     };
