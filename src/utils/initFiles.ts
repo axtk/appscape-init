2f17756387ae2df905df1dc68853e9465d6df4e2
axtk <axtkx@inbox.ru>
2024-12-22 23:51:28 +0300

    move demo to presets/blank

--- src/utils/initFiles.ts
+++ src/utils/initFiles.ts
@@ -3,7 +3,7 @@ import {join} from 'node:path';
 import type {Config} from '../types/Config';
 
 export async function initFiles({ownDir, targetDir}: Config) {
-    let items = await readdir(join(ownDir, 'demo'));
+    let items = await readdir(join(ownDir, 'presets/blank'));
 
     await Promise.all(
         items.map(item => {
@@ -12,7 +12,7 @@ export async function initFiles({ownDir, targetDir}: Config) {
                 : item;
 
             return cp(
-                join(ownDir, 'demo', item),
+                join(ownDir, 'presets/blank', item),
                 join(targetDir, targetItem),
                 {recursive: true},
             );
