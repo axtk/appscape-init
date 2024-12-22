25ebc2e49995a3125d44f86e80a784f8ac5a17b6
axtk <axtkx@inbox.ru>
2024-12-22 23:55:01 +0300

    add files preset

---
+++ presets/files/src/entries/main/server.ts
@@ -0,0 +1,11 @@
+import {Router, static as expressStatic} from 'express';
+import {dir} from 'appscape';
+
+export let server = Router();
+
+server.use('/main', expressStatic('src/entries/main/public'));
+
+server.get('/:name?', dir({
+    path: 'src/entries/main/content',
+    index: 'index',
+}));
